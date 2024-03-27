204ec5fcb7eb2ee2ec0c2b195f45ae9f3aa5efd5
iim7md11 <iim7md11thespace@gmail.com>
2024-03-27 12:28:26 +0200

    Update

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -6,16 +6,16 @@ const SocialMedia = () => {
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-md-6 text-center">
-                        <a href="/" className="text-dark mb-4 icon-link animate__animated animate__fadeInLeft" style={{ marginRight: '20px' }}> {/* Added animation classes */}
+                        <a href="https://discord.com/users/1091118468155314306" className="text-dark mb-4 icon-link animate__animated animate__fadeInLeft" style={{ marginRight: '20px' }}> {/* Added animation classes */}
                             <i className="fab fa-discord" style={{ fontSize: '3.5rem' }}></i> {/* Decreased icon size */}
                         </a>
-                        <a href="/" className="text-dark mb-4 icon-link animate__animated animate__fadeInUp" style={{ marginRight: '20px' }}> {/* Added animation classes */}
+                        <a href="https://twitter.com/iim7md11_" className="text-dark mb-4 icon-link animate__animated animate__fadeInUp" style={{ marginRight: '20px' }}> {/* Added animation classes */}
                             <i className="fab fa-xing-square" style={{ fontSize: '3.5rem' }}></i> {/* Decreased icon size */}
                         </a>
-                        <a href="/" className="text-dark mb-4 icon-link animate__animated animate__fadeInRight" style={{ marginRight: '20px' }}> {/* Added animation classes */}
+                        <a href="https://github.com/iim7md11" className="text-dark mb-4 icon-link animate__animated animate__fadeInRight" style={{ marginRight: '20px' }}> {/* Added animation classes */}
                             <i className="fab fa-github-square" style={{ fontSize: '3.5rem' }}></i> {/* Decreased icon size */}
                         </a>
-                        <a href="/" className="text-dark mb-4 icon-link animate__animated animate__fadeInDown"> {/* Added animation classes */}
+                        <a href="https://www.instagram.com/iim7md11_/" className="text-dark mb-4 icon-link animate__animated animate__fadeInDown"> {/* Added animation classes */}
                             <i className="fab fa-instagram-square" style={{ fontSize: '3.5rem' }}></i> {/* Decreased icon size */}
                         </a>
                     </div>
